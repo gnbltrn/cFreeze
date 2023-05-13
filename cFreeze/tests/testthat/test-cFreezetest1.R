839382a15f66f78c5c83e39d59a5f63bced1c8bf
gnbltrn <123510055+gnbltrn@users.noreply.github.com>
2023-05-13 03:58:16 -0500

    tiny change

--- cFreeze/tests/testthat/test-cFreezetest1.R
+++ cFreeze/tests/testthat/test-cFreezetest1.R
@@ -2,6 +2,7 @@ test_that("cFreeze::freeze returns a percent > 0", {
   expect_gt(test_data <- cFreeze::freeze(-118, 43, 9, 29), 0)
 })
 
+
 test_that("cFreeze::freeze returns a percent < = 100", {
   expect_lte(test_data <- cFreeze::freeze(-118, 43, 9, 29), 100)
 })  
